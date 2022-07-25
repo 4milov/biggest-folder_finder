46999a9fdaea14c25470e1ba36c28697223fce72
4milo <4milov@gmail.com>
2022-07-26 01:49:11 +0300

    add using ForkJoinPool and time of completion

--- Test/BiggestFolderFinder/src/Main.java
+++ Test/BiggestFolderFinder/src/Main.java
@@ -1,13 +1,31 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
+
         String folderPath = "D:/разбор";
         File file = new File(folderPath);
+
+        long start1 = System.currentTimeMillis();
+
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+
+        long duration1 = System.currentTimeMillis() - start1;
+
+
+
         long sum = 0;
 
-//        System.out.println(file.length());
+        long start2 = System.currentTimeMillis();
         System.out.println(getFolderSize(file));
+        long duration2 = System.currentTimeMillis() - start2;
+
+        System.out.println("ForkJoinPool: " + duration1 +"\n"
+                + "getFolderSize: " + duration2);
     }
 
     public static long getFolderSize(File folder) {
