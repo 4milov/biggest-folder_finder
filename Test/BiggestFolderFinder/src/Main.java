64639f9586b1feb65b7ed6b7fac29a24bbb59f33
4milo <4milov@gmail.com>
2022-07-28 01:00:07 +0300

    add methods getSizeFromHumanReadable and getHumanReadableSize add comments in class Main

--- Test/BiggestFolderFinder/src/Main.java
+++ Test/BiggestFolderFinder/src/Main.java
@@ -1,12 +1,18 @@
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Collectors;
 
 public class Main {
+    private final static char[] sizeMultipiers = {'B', 'K','M', 'G', 'T'};
+
     public static void main(String[] args) {
 
         String folderPath = "D:/разбор";
         File file = new File(folderPath);
 
+        //Сравнение длительности выполнения методов ForkJoinPool и getFolderSize
         long start1 = System.currentTimeMillis();
 
         FolderSizeCalculator calculator = new FolderSizeCalculator(file);
@@ -26,8 +32,14 @@ public class Main {
 
         System.out.println("ForkJoinPool: " + duration1 +"\n"
                 + "getFolderSize: " + duration2);
+
+        System.out.println(getSizeFromHumanReadable("235K")); // Из Кб переводит в байты
+        System.out.println(getHumanReadableSize(240640)); // Из байтов переводит в килобайты
+
+
     }
 
+    //Получение размера папки
     public static long getFolderSize(File folder) {
         if (folder.isFile()) {
             return folder.length();
@@ -40,5 +52,32 @@ public class Main {
             sum += getFolderSize(file);
         }
         return sum;
+    }
+
+    //Байты в Кб, Мб, Гб
+    public static String getHumanReadableSize(long size){
+        for(int i = 0; i < sizeMultipiers.length; i++){
+            double value = size / Math.pow(1024, i);
+            if(value < 1024) {
+                return Math.round(value) + "" + sizeMultipiers[i] + (i>0 ? "b" : "");
+            }
+        }
+        return "Very big!";
+    }
+
+    //B, Kb, Mb, Gb,Tb в байты
+    public static long getSizeFromHumanReadable(String size){
+        HashMap<Character, Integer> char2multiplier = getMultipliers();
+        char sizeFactor = size.replaceAll("[0-9\\s+]+","").charAt(0);
+        int multiplier = char2multiplier.get(sizeFactor);
+        return multiplier * Long.parseLong(size.replaceAll("[^0-9]",""));
+    }
+
+    //Словарь для getSizeFromHumanReadable
+    private static HashMap<Character, Integer> getMultipliers(){
+        HashMap<Character, Integer> char2multiplier = new HashMap<>();
+        for(int i = 0; i < sizeMultipiers.length; i++){
+            char2multiplier.put(sizeMultipiers[i], (int) Math.pow(1024,i));
+        } return char2multiplier;
     }
 }
