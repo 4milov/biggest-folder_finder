107bcd59c5f6caf86ead194fc5610110828cfa35
4milo <4milov@gmail.com>
2022-08-10 00:42:54 +0300

    add comments in class FolderSizeCalculator

--- Test/BiggestFolderFinder/src/FolderSizeCalculator.java
+++ Test/BiggestFolderFinder/src/FolderSizeCalculator.java
@@ -11,21 +11,21 @@ public class FolderSizeCalculator extends RecursiveTask<Long> {
 
     @Override
     protected Long compute(){
-        if (folder.isFile()){
+        if (folder.isFile()){  //если файл, то возвращаем размер файла
             return folder.length();
         }
         long sum = 0;
-        List<FolderSizeCalculator> subTask = new LinkedList<>(); //подзадачи в список
-        File[] files = folder.listFiles();
+        List<FolderSizeCalculator> subTask = new LinkedList<>(); //запоминаем задачи в связанный список
+        File[] files = folder.listFiles(); // добавляем в массив files список файлов
         assert files != null;
         for(File file : files){
-            FolderSizeCalculator task = new FolderSizeCalculator(file);
-            task.fork(); // ассинхронный запуск задачи
-            subTask.add(task); // добавляем в список подзадач
+            FolderSizeCalculator task = new FolderSizeCalculator(file); //проходим по каждому файлу из списка и вызываем для него FolderSizeCalculator
+            task.fork(); // асинхронный запуск задачи - в разные потоки
+            subTask.add(task); // добавляем в список подзадач, который был создан выше
         }
 
         for(FolderSizeCalculator task : subTask){
-            sum+= task.join(); //дожидается выполнения задачи(в потоке) и возвращает результат ее выполнения
+            sum+= task.join(); //дожидается выполнения задачи(в потоке) и возвращает результат ее выполнения (сумма)
         }
         return sum;
     }
