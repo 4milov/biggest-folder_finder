f911ee08e5cbcce084a30ba5bb60085efe45256a
4milo <4milov@gmail.com>
2022-08-12 00:31:53 +0300

    Print folder like tree

--- Test/BiggestFolderFinder/src/Node.java
+++ Test/BiggestFolderFinder/src/Node.java
@@ -8,6 +8,8 @@ public class Node {
     private ArrayList<Node> children;
     private long size;
 
+    private int level;
+
     public Node(File folder){
         this.folder = folder;
         children = new ArrayList<>();
@@ -18,6 +20,7 @@ public class Node {
     }
 
     public void addChild(Node node){
+        node.setLevel(level + 1);
         children.add(node);
     }
 
@@ -39,9 +42,13 @@ public class Node {
         String size = SizeCalculator.getHumanReadableSize(getSize());
         builder.append(folder.getName()).append(" - ").append(size).append("\n");
         for (Node child : children) {
-            builder.append("  ").append(child.toString());
+            builder.append("  ".repeat(this.level)).append(child.toString());
         }
         return builder.toString();
+    }
+
+    private void setLevel(int level) {
+        this.level = level;
     }
 
 
